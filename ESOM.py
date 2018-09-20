fe3ad51c6875d9428f1f129cb2df1d30a178c977
m <mark.alex@web.de>
2018-09-20 15:42:21 +0200

    python package complete

--- ESOM.py
+++ ESOM.py
@@ -1,20 +1,32 @@
 from Node import Node
-import matplotlib.pyplot as plt
 import pandas as pd
 from PIL import Image
 import numpy as np
 import seaborn as sns
 
-class Grid:
-    def __init__(self, x = 60, y = 100, attributes = 4):
+
+class ESOM:
+    """
+    ESOM implementation after hearing Knowledge Discovery at Marburg University
+    Currently the __init__ consumes all data and plots an ESOM using Euklidian distance
+    """
+
+    def __init__(self, trainingDF, categoriesVector, x=5, y=10, generations = 1, frameSize = 10):
+        self.categoriesVector = categoriesVector
+        self.dataTraining = trainingDF
         self.x = x
         self.y = y
-        self.nodes = np.array([[Node(i, j, attributes) for j in range(y)] for i in range(x)])
-        self.attributes = attributes
+        self.nodes = np.array([[Node(i, j, trainingDF.shape[1]) for j in range(y)] for i in range(x)])
         self.timeWeight = 1
-        self.maxDistance = self.dist(Node(0,0,0), Node(self.x/2, self.y/2, 0))
+        self.maxDistance = self.__dist(Node(0, 0, 0), Node(self.x / 2, self.y / 2, 0))
+        self.generations = generations
 
-    def dist(self, nodeA, nodeB):
+        # May be called outside __init__ in the future
+        self.consume()
+        self.plot(frameSize = frameSize)
+        #
+
+    def __dist(self, nodeA, nodeB):
         def toroidalDistanceAlongAxis(a, b, axisLength):
             straightDistance = abs(a - b)
             aroundTorusDistance = abs(abs(a - b) - axisLength)
@@ -22,25 +34,31 @@ class Grid:
 
         dx = toroidalDistanceAlongAxis(nodeA.x, nodeB.x, self.x)
         dy = toroidalDistanceAlongAxis(nodeA.y, nodeB.y, self.y)
-        return (dx**2+dy**2)**0.5
+        return (dx ** 2 + dy ** 2) ** 0.5
 
-    def consume(self, vector):
+    def consume(self):
+        for j in range(self.generations):
+            for i in self.dataTraining.index:
+                self.__consumeVector(self.dataTraining.iloc[i].values)
+            print("Finished generation {} of {} ({:0.0f}%)".format(j + 1, self.generations, 100*(j + 1)/self.generations))
+
+    def __consumeVector(self, vector):
         radiusOfInfluence = 1
 
         def propagateNeighbourhood(bmu, vector):
             for row in self.nodes:
                 for node in row:
-                    distBMU = self.dist(bmu, node)/ self.maxDistance  # 0 to 1
+                    distBMU = self.__dist(bmu, node) / self.maxDistance  # 0 to 1
                     if distBMU < radiusOfInfluence:
-                        distWeight = (1 - distBMU)**4 #Cone neighbourhood function of unlimited radius of influence
+                        distWeight = (1 - distBMU) ** 4  # Cone neighbourhood function of unlimited radius of influence
                         node.vector += (vector - node.vector) * distWeight * self.timeWeight
-        assert len(vector) == self.attributes, "Vector doesnt have expected dimensions"
-        bmu = self.getBMU(vector)
+
+        assert len(vector) == self.dataTraining.shape[1], "Vector doesnt have expected dimensions"
+        bmu = self.__getBMU(vector)
         propagateNeighbourhood(bmu, vector)
         self.timeWeight *= 0.98  # Deceasing the time weight at every datapoint
 
-
-    def getBMU(self, vector):
+    def __getBMU(self, vector):
         bmuSimilarity = -99999
         bmu = None
         for row in self.nodes:
@@ -51,7 +69,7 @@ class Grid:
         assert bmu is not None, "no best matching unit found"
         return bmu
 
-    def plot(self, ncolors = 20, frameSize = 10):
+    def plot(self, ncolors=20, frameSize=10):
         xDim = self.x * frameSize
         yDim = self.y * frameSize
         palette = sns.color_palette("RdBu", n_colors=ncolors)
@@ -82,7 +100,7 @@ class Grid:
             totalDistance = 0
             totalDistance += node.dist(self.nodes[getXTorusCoordinate(x + 1)][getYTorusCoordinate(y + 1)].vector)
             totalDistance += node.dist(self.nodes[getXTorusCoordinate(x + 1)][getYTorusCoordinate(y)].vector)
-            totalDistance += node.dist(self.nodes[getXTorusCoordinate(x + 1)][getYTorusCoordinate(y-1)].vector)
+            totalDistance += node.dist(self.nodes[getXTorusCoordinate(x + 1)][getYTorusCoordinate(y - 1)].vector)
             totalDistance += node.dist(self.nodes[getXTorusCoordinate(x)][getYTorusCoordinate(y + 1)].vector)
             totalDistance += node.dist(self.nodes[getXTorusCoordinate(x)][getYTorusCoordinate(y - 1)].vector)
             totalDistance += node.dist(self.nodes[getXTorusCoordinate(x - 1)][getYTorusCoordinate(y + 1)].vector)
@@ -96,10 +114,10 @@ class Grid:
 
         for i, row in enumerate(self.nodes):
             for j, node in enumerate(row):
-                nodeColorIDX = int(((nearestNeighbourDistance(node) - min)/(max-min)) * (ncolors-1))
+                nodeColorIDX = int(((nearestNeighbourDistance(node) - min) / (max - min)) * (ncolors - 1))
                 # Nearest neighbour hyperspace distance
 
-                assert nodeColorIDX >=0 and nodeColorIDX < 20, nodeColorIDX
+                assert nodeColorIDX >= 0 and nodeColorIDX < 20, nodeColorIDX
                 nodeColorIDX = ncolors - 1 - nodeColorIDX
                 try:
                     color(pixels, i, j, frameSize, palette[nodeColorIDX])
@@ -109,18 +127,18 @@ class Grid:
 
         def drawBMU(pixels, x, y, color):
             for i in range(frameSize):
-                pixels[x*frameSize + i, y*frameSize + i] = color
-                pixels[x*frameSize + i, y*frameSize + frameSize - 1 - i] = color
+                pixels[x * frameSize + i, y * frameSize + i] = color
+                pixels[x * frameSize + i, y * frameSize + frameSize - 1 - i] = color
 
         def drawBMUs():
-            for i in normalised.index:
-                species = dataAll.iloc[i]['species']
-                color = (255,0,0) if species == 'setosa'\
-                else (0,255,0) if species == 'virginica' \
-                else (0,0,255) if species == "versicolor"\
-                else None
+            for i in self.dataTraining.index:
+                species = self.categoriesVector[i]
+                color = (255, 0, 0) if species == 'setosa' \
+                    else (0, 255, 0) if species == 'virginica' \
+                    else (0, 0, 255) if species == "versicolor" \
+                    else None
 
-                node = g.getBMU(normalised.iloc[i].values)
+                node = self.__getBMU(self.dataTraining.iloc[i].values)
                 drawBMU(pixels, node.x, node.y, color)
 
         drawBMUs()
@@ -138,17 +156,16 @@ class Grid:
     def __repr__(self):
         return self.__str__()
 
+
 if __name__ == "__main__":
     dataAll = pd.read_csv('iris.csv')
     dataTrain = dataAll.filter(["sepal_length", "sepal_width", "petal_length", "petal_width"])
+
     def normalise(df):
         for col in df.columns:
             df[col] -= df[col].min()
             df[col] /= df[col].max()
         return df
+
     normalised = normalise(dataTrain)
-    g = Grid()
-    for j in range(10):
-        for i in normalised.index:
-            g.consume(normalised.iloc[i].values)
-    g.plot()
+    ESOM(normalised, dataAll['species'])
--- GridTest.py
+++ GridTest.py
@@ -1,23 +1,23 @@
 import numpy as np
-from Grid import Grid
+from ESOM import ESOM
 
 # TESTING
 def torodialDistTest():
-    grid = Grid()
+    grid = ESOM()
     print("-----------------------  TEST  -----------------------")
     print("\nAround Torus Examples:\n")
     print("Torus dimensions x={}, y={}".format(grid.x, grid.y))
     first = grid.nodes[0][0]
     second = grid.nodes[19][0]
     print('For Nodes (0,0) and (19,0) the Euklidian distance is:')
-    dist = grid.dist(first, second)
+    dist = grid.__dist(first, second)
     print(dist)
     assert (dist == 1)
 
     first = grid.nodes[1][1]
     second = grid.nodes[1][25]
     print('For Nodes (1,1) and (1,25) the Euklidian distance is:')
-    dist = grid.dist(first, second)
+    dist = grid.__dist(first, second)
     print(dist)
     assert (dist == 6)
 
@@ -26,40 +26,40 @@ def torodialDistTest():
     first = grid.nodes[1][1]
     second = grid.nodes[3][3]
     print('For Nodes (1,1) and (3,3) the Euklidian distance is:')
-    dist = grid.dist(first, second)
+    dist = grid.__dist(first, second)
     print('{0:.2f}'.format(dist))
     print("------------------------------------------------------")
 
 
 def bmuTest():
-    g = Grid()
+    g = ESOM()
     print("-----------------------  TEST  -----------------------")
     print("BMU to [1,1,1,1] should be full of high values:")
-    print(g.getBMU(np.array([1, 1, 1, 1])))
+    print(g.__getBMU(np.array([1, 1, 1, 1])))
     print("------------------------------------------------------")
 
 
 def consumeDataPointTest():
     print("-----------------------  TEST  -----------------------")
-    g = Grid()
+    g = ESOM()
     datapoint = np.array([1, 1, 1, 1])
     print("Best Matching unit to datapoint {}:".format(datapoint))
-    print(g.getBMU(datapoint))
+    print(g.__getBMU(datapoint))
     print("Consume {}".format(datapoint))
     g.consume(datapoint)
     print("Should be same coordinates, but BMU == {}".format(datapoint))
-    bmu = g.getBMU(datapoint)
+    bmu = g.__getBMU(datapoint)
     assert all(bmu.vector.__eq__(datapoint)), "Test failed, BMU != datapoint after first insertion"
     print(bmu)
     print("\n")
 
     datapoint = np.array([0, 0, 0, 0])
     print("Best Matching unit to datapoint {}:".format(datapoint))
-    print(g.getBMU(datapoint))
+    print(g.__getBMU(datapoint))
     print("Consume {}".format(datapoint))
     g.consume(datapoint)
     print("Should be same coordinates, and BMU approaching but not equal to {}".format(datapoint))
-    print(g.getBMU(datapoint))
+    print(g.__getBMU(datapoint))
     print("------------------------------------------------------")
 
 
